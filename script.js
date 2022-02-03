24c0d91b2f2484d0e3e57917188ab2aa99369420
SagaMS <SagaMS@users.noreply.github.com>
2022-02-03 14:51:36 +0700

    in row

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const card = document.querySelector('.card__inner');
+const card = document.querySelectorAll('.card__inner');
+
+
+
+for (let i = 0; i < card.length; i++) {
+
+    card[i].addEventListener('click', function () {
+
+        card[i].classList.toggle('is-flipped');
+    });
+}
+
+
+
+// card.addEventListener('click', function () {
+
+//    card.classList.toggle('is-flipped');
+// });
